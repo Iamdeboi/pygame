f9402caded707820f876d025264012cbdde4880b
Iamdeboi <dylandelsontro@gmail.com>
2024-10-06 18:29:16 -0400

    G: Added "A" and "D" rotation keys to player

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import pygame
 from constants import *
 from player import Player
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-dt = 0
+
 
 def main():
     pygame.init()
-    pygame.time.Clock
+    dt = 0
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.update(dt) # Call player.update with dt
         player.draw(screen)
         pygame.display.flip()
         
