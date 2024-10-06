672522cc85d02f3c761c3d5f378717b56cfd5cb3
Iamdeboi <dylandelsontro@gmail.com>
2024-10-06 17:39:51 -0400

    F: Added Player visual and draw screen methods.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from constants import *
+from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 dt = 0
 
 def main():
@@ -15,7 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS (returns as milliseconds, divide by 1000 to get seconds)
