f0a8d05d303076d2d60253ea0b520b8919c36510
Iamdeboi <dylandelsontro@gmail.com>
2024-10-06 17:14:19 -0400

    E: Added 60 FPS as the frame limit for game

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pygame
 from constants import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     pygame.init()
+    pygame.time.Clock
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +17,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS (returns as milliseconds, divide by 1000 to get seconds)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
