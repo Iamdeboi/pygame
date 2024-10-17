3560d3640cf5ce1c27ddbe54bd5e99f20b8a6c78
Iamdeboi <dylandelsontro@gmail.com>
2024-10-17 11:40:02 -0400

    L: Added Shot class, shooting functionality, relative constants for PLAYER_SHOOT_SPEED and SHOT_RADIUS. Added new "shots" group in initialization code (main.py).

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 
@@ -16,8 +17,10 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updateable, drawable)
+    Shot.containers = (shots, updateable, drawable)
     AsteroidField.containers = updateable
     asteroid_field = AsteroidField()
 
